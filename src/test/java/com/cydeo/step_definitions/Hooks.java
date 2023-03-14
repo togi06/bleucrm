49306b056523aca7d8febb6d01cd8a0e6fa44fa0
togi06 <t_bayter_06@hotmail.com>
2023-03-14 22:30:30 +0100

    Hooks and DriverClose

---
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -0,0 +1,54 @@
+package com.cydeo.step_definitions;
+
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.Driver;
+import io.cucumber.java.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hooks {
+
+    @Before
+    public void  setupScenario(){
+        System.out.println("====Setting up browser using cucumber @Before");
+    }
+
+    @Before
+    public void  setupScenarioForLogins(){
+        System.out.println("====this will only apply to scenarios with @login tag");
+    }
+
+    @Before
+    public void  setupForDatabaseScenarios(){
+        System.out.println("====this will only apply to scenarios with @tb tag");
+    }
+
+    @After
+    public void teardownScenario(Scenario scenario){
+
+
+
+        if (scenario.isFailed()){
+            byte [] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png",scenario.getName());
+
+        }
+
+        //BrowserUtils.sleep(5);
+        Driver.closeDriver();
+
+    }
+
+    @BeforeStep
+    public void setupStep(){
+        System.out.println("--------> applying setup using @BeforeStep");
+    }
+
+    @AfterStep
+    public void afterStep(){
+        System.out.println("--------> applying teardown using @AfterStep");
+
+    }
+
+
+}
--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -41,7 +41,13 @@ public class Driver {
         return  driverPool.get();
     }
 
+    public static void closeDriver(){
+        if (driverPool.get() != null){
+            driverPool.get().quit();// this line will terminate the existing session. value will not even be null
+            driverPool.remove();
+        }
 
+    }
 
 
 }
